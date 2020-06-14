e4c215db1db9b35541ff641eae571aea98a18c99
Tiffanygray2383 <63433432+Tiffanygray2383@users.noreply.github.com>
2020-06-14 15:44:27 -0400

    week 1 late assignment

---
+++ test.js
@@ -0,0 +1,18 @@
+console.log ("I am running javascript with node"
+)
+var marketlist = {
+   dairy: ["eggs", "milk"],// array
+   meat: "chicken beef seafood",
+   beverages: "juice soda wine water", 
+number: 9,
+ dairy2: function () {
+    return this.eggs + " " + this.milk;
+  },
+
+
+    boolean: true    
+
+
+
+}
+
